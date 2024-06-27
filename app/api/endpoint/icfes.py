1079cbad661b6e947f0687bd02e0cbfdfc22f3f9
Carlos Sanchez Carrascal <carlosstqm2225@gmail.com>
2024-06-27 11:01:06 -0500

    more changes icfes

--- app/api/endpoint/icfes.py
+++ app/api/endpoint/icfes.py
@@ -1,7 +1,36 @@
 from fastapi import APIRouter
+import requests
 
 icfes_router = APIRouter()
 
+
 @icfes_router.get("/icfes")
-async def get_icfes():
-    return {"message": "Hello World"}
+async def first_request(doc, reg, td):
+    data = {
+        "tipoDocumento": td.upper(),
+        "numeroDocumento": doc,
+        "fechaNacimiento": None,
+        "identificacionUnica": reg.upper(),
+    }
+    try:
+        res = requests.post(
+            "https://resultadosbackendoci.icfes.edu.co/api/segurity/autenticacionResultados",
+            json=data,
+        ).json()
+        print("response data -->", res)
+        print("request")
+        try:
+            fechaNacimiento = (
+                res.get("datosAutenticacion")[0]
+                .get("datosParametros")
+                .get("fechaNacimiento")
+            )
+        except:
+            return "bad"
+
+        token = res.get("token")
+        res = requests.get("", headers={"Authorization": token}).json()
+
+    except:
+        print("error")
+        return "bad"
